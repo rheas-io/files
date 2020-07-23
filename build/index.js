016e3e9967d172dc1e3a92ca97c512907a4d6ed2
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-07-23 23:27:06 +0530

    es2017 build

--- build/index.js
+++ build/index.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.FileManager = void 0;
-var fileManager_1 = require("./fileManager");
+const fileManager_1 = require("./fileManager");
 Object.defineProperty(exports, "FileManager", { enumerable: true, get: function () { return fileManager_1.FileManager; } });
