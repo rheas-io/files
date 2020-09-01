fe3cf37a6c86b571fcecb169e127be58b65dbe86
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-09-02 00:20:21 +0530

    removed build dir from git

--- build/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { FileManager } from "./fileManager";
-export { FileManager };
--- build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FileManager = void 0;
-const fileManager_1 = require("./fileManager");
-Object.defineProperty(exports, "FileManager", { enumerable: true, get: function () { return fileManager_1.FileManager; } });
