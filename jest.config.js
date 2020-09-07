90610c1c0d5a44bb796cfa12a651858ad8c0a446
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-09-07 14:09:53 +0530

    jest transform syntax updated

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  transform: {
-    "^.+\\.ts?$": "ts-jest",
-  },
-  testEnvironment: "node",
-  testRegex: "/tests/.*\\.(test|spec)?\\.(ts|tsx)$",
-  collectCoverageFrom: ["src/**/*.{ts,js}"],
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    transform: {
+        '^.+.tsx?$': 'ts-jest',
+    },
+    testEnvironment: 'node',
+    testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
+    collectCoverageFrom: ['src/**/*.{ts,js}'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
